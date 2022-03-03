3bd0c588d586ef0bc603d41b3afae30f5bdd5197
adhiponnuzz <79753378+adhiponnuzz@users.noreply.github.com>
2022-03-03 10:46:21 +0530

    Update sentimentalanalysis.py

--- sentimentalanalysis.py
+++ sentimentalanalysis.py
@@ -1,15 +1,21 @@
-from nltk.corpus.reader import tagged
 import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import sent_tokenize,word_tokenize
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-sw=set(stopwords.words('english'))
-txt="Hello.I am Darsana Prasad.I am a native of Athani."
-tokenized=nltk.word_tokenize(txt)
-for i in tokenized:
-  wordlist=word_tokenize(i)
-  wordlist=[w for w in wordlist if not w in sw]
-  tagged=nltk.pos_tag(wordlist)
-  print(tagged)
+from nltk.corpus import movie_reviews
+nltk.download('movie_reviews')
+import random
+documents =[(list(movie_reviews.words(fileid)),category)
+              for category in movie_reviews.categories()
+              for fileid in movie_reviews.fileids(category)]
+random.shuffle(documents)
+all_words=nltk.FreqDist(w.lower()for w in movie_reviews.words())
+word_features=list(all_words)[:2000]
+def document_features(document):
+  document_words=set(document)
+  features={}
+  for word in word_features:
+    features['Contains({})'.format(word)]=(word in document_words)
+  return features
+featuresets = [(document_features(d),c) for (d,c) in documents]
+train_set, test_set = featuresets[100:], featuresets[:100]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print(nltk.classify.accuracy(classifier, test_set))
+classifier.show_most_informative_features(5)
